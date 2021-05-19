1f3e64cbb3cd932b11c34fc2472f26d65dac50d9
Ashish Kumar <ashishpurbey900@gamil.com>
2021-05-19 10:07:08 +0530

    almost completed version

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -32,7 +32,7 @@ const Header = () => {
       >
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand href='/'>ProShop</Navbar.Brand>
+            <Navbar.Brand href='/'>AZADSHOP</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
